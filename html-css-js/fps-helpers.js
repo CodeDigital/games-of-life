3541b869f3d6c03ea99494381fa00e039046adf9
xtravers <legoxavierlocketravers@gmail.com>
2024-03-23 15:52:06 +1100

    commenting on fps-helpers.js

--- html-css-js/fps-helpers.js
+++ html-css-js/fps-helpers.js
@@ -1,10 +1,21 @@
 const fpsOptions = [1/5,1,2,5,10,30,60,120]
 let fpsIndex = 3
 
+/**
+ * Return the fps based on the fpsIndex variable
+ *
+ * @return {number} 
+ */
 function fps() {
     return fpsOptions[fpsIndex]
 }
 
+/**
+ * Update the index of FPS.
+ * Also update the FPS text based on the new index.
+ *
+ * @param {int} deltaIndex
+ */
 function updateFPS(deltaIndex){
     fpsIndex = Math.max(
         0, 
